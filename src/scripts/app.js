eada8a7db6e68448332f997d0e38cd53864cb64d
RossiJr <jfrjunior@sga.pucminas.br>
2021-05-16 22:29:57 -0300

    Versao Inicial 0.0.2

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,26 @@
-const projName = 'TI - AW';
+/**
+ * 
+ * @param {HTMLElement} idDoSite 
+ */
+function mudarCorDeFundo(idDoSite){
+    const nota = idDoSite.getAttribute('data-nota');
+    if(nota < 4){
+        idDoSite.style.backgroundColor = '#FF0000';
+    }
+    else if (nota >= 4 && nota<=6){
+        idDoSite.style.backgroundColor = '#FFFF00';
+    }
+    else {
+        idDoSite.style.backgroundColor = '#008000';
+    }
+}
+
+
+function load(){
+    document.querySelectorAll('.botao').forEach(function(a){
+        mudarCorDeFundo(a);
+    })
+}
+
+
+
